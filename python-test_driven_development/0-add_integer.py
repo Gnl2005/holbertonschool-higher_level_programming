0f3183e17e6aa96a5290b8109b32692e909d1124
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-11-08 15:58:14 +0000

    K

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 """
-This is the "add_integer" module.
+This module provides a function that adds two integers.
+It ensures inputs are either integers or floats.
+If inputs are invalid, a TypeError is raised.
+Floats are first casted to integers before addition.
 """
 
 
 def add_integer(a, b=98):
-    """Return the integer addition of a and b."""
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-
-    if (
-        a != a or b != b
-        or a in (float('inf'), float('-inf'))
-        or b in (float('inf'), float('-inf'))
-    ):
-        raise OverflowError("cannot convert float NaN or infinity to integer")
-
-    return (int(a) + int(b))
+        """Add two integers or floats and return the result as integer."""
+            if ((not isinstance(a, int) and not isinstance(a, float))):
+                        raise TypeError("a must be an integer")
+                        if ((not isinstance(b, int) and not isinstance(b, float))):
+                                    raise TypeError("b must be an integer")
+                                    return (int(a) + int(b))
