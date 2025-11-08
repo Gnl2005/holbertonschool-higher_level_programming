ff81a0d0709820563f7525ed2802319cdcc02111
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-11-08 16:10:28 +0000

    K

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,12 +12,12 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    # NaN yoxlaması
-    if a != a or b != b:
-        raise ValueError("cannot convert float NaN to integer")
-    
-    # Infinity yoxlaması
-    if a in (float('inf'), float('-inf')) or b in (float('inf'), float('-inf')):
-        raise OverflowError("cannot convert float infinity to integer")
+    if (a != a or b != b or
+        a in (float('inf'), float('-inf')) or
+        b in (float('inf'), float('-inf'))):
+        if a != a or b != b:
+            raise ValueError("cannot convert float NaN to integer")
+        else:
+            raise OverflowError("cannot convert float infinity to integer")
 
     return int(a) + int(b)
